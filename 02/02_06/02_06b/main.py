bac88b2b039a3627d8d58ad06ce5be4e54ea0231
Murilo Lima <54185216+omurilolima@users.noreply.github.com>
2024-03-19 18:50:23 +0000

    Tuples

--- 02/02_06/02_06b/main.py
+++ 02/02_06/02_06b/main.py
@@ -1,1 +1,15 @@
 # Tuples are immutable array-like structures
+
+my_list = [8, 5, 0, 3, 9, 7]
+ITEM = 8
+
+def search(item, listy):
+  for element in listy:
+    if element == item:
+      return True
+  else:
+    return False
+
+
+ITEM_INDEX = my_list.index(ITEM)
+print(ITEM_INDEX)
