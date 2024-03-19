cee325efe3bef9e026a42dc76941f9408fe21bea
Murilo Lima <54185216+omurilolima@users.noreply.github.com>
2024-03-19 16:46:01 +0000

    Compute the average number of pets

--- 02/02_03/02_03b/main.py
+++ 02/02_03/02_03b/main.py
@@ -6,6 +6,10 @@ has in a given class.
 student_pet_count_list = [0, 1, 0, 2, 1, 1, 4, 0, 0, 0, 3, 2, 1, 3, 0, 2, 2, 4]
 
 NUM_OF_STUDENTS = len(student_pet_count_list)
-print(NUM_OF_STUDENTS)
+SUM = 0
+for s in student_pet_count_list:
+  SUM = SUM + s
+
+print(SUM/NUM_OF_STUDENTS)
 
 # average = sum / number of items
