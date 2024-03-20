312a8cee607c47122e733e284162dbdf902f9649
Murilo Lima <54185216+omurilolima@users.noreply.github.com>
2024-03-20 19:34:41 +0000

    deque as a stack

--- 06/06_03/06_03b/main.py
+++ 06/06_03/06_03b/main.py
@@ -1,1 +1,9 @@
 from collections import deque
+
+history_stack = deque()
+history_stack.append("http://google.com")
+history_stack.append("http://linkedin.com")
+history_stack.append("http://stackoverflow.com")
+
+print(history_stack[-1])
+print(history_stack.pop())
